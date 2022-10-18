751b17e0abb12e829b0e43de556625dba43b8e9a
Alexandre Umpierre <105897054+aumpierre-unb@users.noreply.github.com>
2022-10-18 13:33:08 -0300

    Add files via upload

--- src/interp1.jl
+++ src/interp1.jl
@@ -1,6 +1,6 @@
 function interp1(X, Y, x)
     for i = 1:length(X)-1
-        if X[i] <= x && x < X[i+1]
+        if X[i] <= x <= X[i+1]
             return (Y[i+1] - Y[i]) / (X[i+1] - X[i]) * (x - X[i]) + Y[i]
         end
     end
