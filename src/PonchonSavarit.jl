9675a6053e8548c826ddce738f35dcd5d0e1dfa9
aumpierre-unb <aumpierre@gmail.com>
2023-01-02 13:48:43 -0300

    minor changes

--- src/PonchonSavarit.jl
+++ src/PonchonSavarit.jl
@@ -4,6 +4,12 @@
 the number of theoretical stages of a distillation column 
 using the Ponchon-Savarit method.
 
+Author: Alexandre Umpierre `aumpierre@gmail.com`
+
+Maintainer's repository: `https://github.com/aumpierre-unb/Ponchon-Savarit.jl`
+
+Citation (any version): `DOI 10.5281/zenodo.7218007`
+
 See also: `stages`, `refmin`, `qR2S`.
 """
 module PonchonSavarit
